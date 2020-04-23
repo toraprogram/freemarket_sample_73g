4f14c9042dab15edbc94a041f78c7ff60aa469d4
ryuso0720 <ryu.cit0720@gmail.com>
2020-04-23 13:54:37 +0900

    add routes card

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root 'items#index'
   resources :items do
     resources :comments, only: :create
+  end
+  
+  resources :card, only: [:new, :index, :create, :destroy]
 end
