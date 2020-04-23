7a9874e31d0a64e97238fae7cb51df27d6f10fba
ryuso0720 <ryu.cit0720@gmail.com>
2020-04-23 13:45:54 +0900

    add routes item

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'items#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :items do
+    resources :comments, only: :create
 end
