07627e2ac12a883ec70664432800a3963c4d66eb
ryuso0720 <ryu.cit0720@gmail.com>
2020-04-24 12:45:11 +0900

    comment db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_24_033806) do
+ActiveRecord::Schema.define(version: 2020_04_24_034124) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id"
@@ -53,6 +53,16 @@ ActiveRecord::Schema.define(version: 2020_04_24_033806) do
     t.datetime "updated_at", null: false
     t.index ["brand_id"], name: "index_category_brands_on_brand_id"
     t.index ["category_id"], name: "index_category_brands_on_category_id"
+  end
+
+  create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "text", null: false
+    t.bigint "user_id"
+    t.bigint "item_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_comments_on_item_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -120,6 +130,8 @@ ActiveRecord::Schema.define(version: 2020_04_24_033806) do
   add_foreign_key "cards", "users"
   add_foreign_key "category_brands", "brands"
   add_foreign_key "category_brands", "categories"
+  add_foreign_key "comments", "items"
+  add_foreign_key "comments", "users"
   add_foreign_key "images", "items"
   add_foreign_key "items", "brands"
   add_foreign_key "items", "categories"
