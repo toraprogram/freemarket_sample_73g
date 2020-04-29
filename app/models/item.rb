24f710987f81ea7b9b4415b0b59598de3e25013b
sayuroy <balotje918@gmail.com>
2020-04-29 09:46:42 +0900

    item create

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,10 +3,11 @@ class Item < ApplicationRecord
   has_many :comment
   has_many :likes, dependent: :destroy
   has_many :images, dependent: :destroy
-  belongs_to :buyer, class_name: "User"
-  belongs_to :seller, class_name: "User"
-  belongs_to :category
-  belongs_to :brand
+  belongs_to :buyer, class_name: "User", optional: true
+  belongs_to :seller, class_name: "User", optional: true
+  belongs_to :category, optional: true
+  belongs_to :brand, optional: true
   accepts_nested_attributes_for :images, allow_destroy: true
 
+  # カテゴリidとブランドidは機能ができたらoptionalを外す
 end
