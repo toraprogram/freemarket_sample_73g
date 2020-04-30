7b560ad0ff0d024b731f888570db3c039e90c12c
toraprogram <tora19911125@gmail.com>
2020-04-30 11:12:38 +0900

    ユーザー新規登録機能の実装<未完成>

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ApplicationRecord
   has_many :items, foreign_key: "saler_id", class_name: "Item"
   has_many :items, foreign_key: "buyer_id", class_name: "Item"
   has_many :orders
+
+  #<他の人に影響が出る可能性を考慮し、コメントアウトしてます。中野>
+  # validates :nickname, presence: true
+  # validates :email, presence: true, uniqueness: true
+  # validates :password, presence: true, length: { minimum: 8}
+  # validates :family_name, presence: true
+  # validates :first_name, presence: true
+  # validates :family_name_kana, presence: true
+  # validates :first_name_kana, presence: true
+  # validates :birthday, presence: true
+  # validates :phone_authy, presence: true, length: { is: 11}
 end
