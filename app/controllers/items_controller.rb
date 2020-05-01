95362a4330cd9abfd8c179771f4b883ba12cb1ca
sayuroy <balotje918@gmail.com>
2020-05-01 13:57:43 +0900

    sellitem validate model

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,20 +4,25 @@ class ItemsController < ApplicationController
   
   def new
     @item = Item.new
+    @item.images.build
   end
 
   def create
     @item = Item.new(item_params)
+    form_error_check(@item)
     if @item.save
       redirect_to root_path
     else
+      
       render :new
     end
     
   end
   
   def show
+
   end
+
 
 
 
@@ -25,8 +30,14 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :condition, :delivery_charge, :delivery_day, :size, :region, :category_id, :brand_id).merge(seller_id: 1, state: true)
-    # .merge(seller_id: current_user.id)ユーザーデータできたら修正する
+    params.require(:item).permit(:name, :price, :description, :condition, :delivery_charge, :delivery_day, :size, :prefecture_id, :category_id, :brand_id, images_attributes: [:image, :image_cache]).merge(seller_id: 1, state: true)
+    # .merge(seller_id: current_user.id)ユーザーデータできたら修正する, imageできたら追加する
   end
 
+  def form_error_check(item)
+    if item[:prefecture_id] == 0
+      item[:prefecture_id] = nil
+    end
+
+  end
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,8 @@
 class Image < ApplicationRecord
 
   belongs_to :item
+  mount_uploader :image, ImageUploader
+
+  validates :image, presence: true
+
 end
