04a3d6a962a210b716ff38bd8f90b64c31f98cc6
4145bjc <bjc4145@yahoo.co.jp>
2020-05-07 09:56:48 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -59,8 +59,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:name, :price, :description, :condition, :delivery_charge, :delivery_day, :size, :prefecture_id, :category_id, :brand_id, images_attributes: [:image, :image_cache]).merge(seller_id: 1, state: true)
-    # .merge(seller_id: current_user.id)ユーザーデータできたら修正する, imageできたら追加する
+    params.require(:item).permit(:name, :price, :description, :condition, :delivery_charge, :delivery_day, :size, :prefecture_id, :category_id, :brand_id, images_attributes: [:image, :image_cache]).merge(seller_id: current_user.id)
   end
 
 end
