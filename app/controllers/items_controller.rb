1f209acb94239379daa87a21a4aec71a18529164
pikarunrun <62536501+pikarunrun@users.noreply.github.com>
2020-05-08 12:49:22 +0900

    sold outの表記　itemコントローラー:newの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   
   
   before_action :set_find,only:[:show, :destroy]
+  before_action :move_to_session, only: [:buycheck, :payment, :new]
   def index
     @items = Item.includes(:images, :category, :seller).order(created_at: :desc) 
     @parents = Category.all.order("id ASC").limit(13)
